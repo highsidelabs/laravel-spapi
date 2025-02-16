6b1d9d1da047e5a64f2bb5e9f5bd28c1e8ec3c29
jesse <jesse@jesseevers.com>
2025-02-16 17:12:27 -0500

    Increase cache key and value size

--- database/migrations/2024_09_11_135400_increase_cache_key_and_value_size.php
+++ database/migrations/2024_09_11_135400_increase_cache_key_and_value_size.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,7 @@ return new class extends Migration
     {
         Schema::table('cache', function (Blueprint $table) {
             $table->string('key', 511)->change();
-            $table->string('value', 1023)->change();
+            $table->string('value', 2559)->change();
         });
     }
 
